6027521a8c631987edb2fa15d31923c01b9350a2
isa <admin@limitsizbilgi.com>
2018-02-01 17:11:35 +0300

    index
    update

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 <?php 
 	if(!isset($_GET['url'])){ exit(); }
 	try {
-		$chunklist=file_get_contents($_GET['url']);
+		$url=file_get_contents($_GET['url']);
 	} catch (Exception $e) {
 	 echo '<div style="display:none"><pre>';
 	 print_r($e);
 	 echo '</pre></div>';
 	}
-	if($chunklist){
-		if(strpos($chunklist, 'chunklist')){
-			$pos=strpos($chunklist, 'chunklist');
-			$chunklist=substr($_GET['url'], 0, $pos).trim(substr($chunklist, $pos));
+	if($url){
+		if(strpos($url, 'chunklist')){
+			$url=substr($_GET['url'], 0, strpos($_GET['url'], 'playlist')).trim(substr($url, strpos($url, 'chunklist')));
 		}else{
-			$chunklist=$_GET['url'];
+			$url=$_GET['url'];
 		}
 	}else{
-		$chunklist=$_GET['url'];
+		$url=$_GET['url'];
 	}
-	echo $chunklist;
+	echo $url;
 ?>
