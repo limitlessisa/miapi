660b2e38cfc2becdcffe83d3be5fd38bd44e3ee8
isa <admin@limitsizbilgi.com>
2018-02-07 12:54:07 +0300

    jsonp added
    json result

--- c/index.php
+++ c/index.php
@@ -1,20 +1,31 @@
 <?php 
+	header('Content-Type: application/json'); 
 	if(!isset($_GET['url'])){ exit(); }
+	
+	if($_GET['callback']){// Only JSONP
+		echo $_GET['callback'].'(';
+	}
+
 	try {
 		$url=file_get_contents($_GET['url']);
 	} catch (Exception $e) {
-	 echo '<div style="display:none"><pre>';
-	 print_r($e);
-	 echo '</pre></div>';
+		echo json_encode($e);
 	}
 	if($url){
 		if(strpos($url, 'chunklist')){
 			$url=substr($_GET['url'], 0, strpos($_GET['url'], 'playlist')).trim(substr($url, strpos($url, 'chunklist')));
+			$result=array('status'=>1,'url'=>$url);
 		}else{
 			$url=$_GET['url'];
+			$result=array('status'=>0,'url'=>$url);
 		}
 	}else{
 		$url=$_GET['url'];
 	}
-	echo $url;
+
+	echo json_encode($result);
+	
+	if($_GET['callback']){// Only JSONP
+		echo ')';
+	}
 ?>
